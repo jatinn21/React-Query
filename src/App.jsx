a670df35457976bd9ca5ceee8ead70f86e2970f3
JatinSharma555 <jatin.sharma@ajmerainfotech.com>
2024-12-29 14:17:04 +0530

    Implement routing and layout structure with header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./components/Layout/MainLayout";
+import Home from "./components/Layout/Pages/Home";
+import About from "./components/Layout/Pages/About";
+import Contact from "./components/Layout/Pages/Contact";
 
-function App() {
-  const [count, setCount] = useState(0)
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+  return <RouterProvider router={route}></RouterProvider>;
+};
 
-export default App
+export default App;
